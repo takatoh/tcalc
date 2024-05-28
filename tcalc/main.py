b1ef7adbe32d690341e4846f31636f075cebc499
takatoh <takatoh.m@gmail.com>
2024-05-28 21:47:46 +0900

    tcalc/main.py: New.

---
+++ tcalc/main.py
@@ -0,0 +1,34 @@
+from lark import Lark
+from lark.exceptions import UnexpectedInput
+from os import path
+import sys
+
+
+here = path.dirname(path.abspath(__file__))
+grammer_file = path.join(here, '../grammer/tcalc.lark')
+with open(grammer_file) as f:
+    TCALC_GRAMMER = f.read()
+
+
+class Parser():
+    def __init__(self):
+        self.parser = Lark(TCALC_GRAMMER, start='expr')
+
+    def parse(self, input_data):
+        try:
+            tree = self.parser.parse(input_data)
+        except UnexpectedInput as e:
+            print('Syntax error:')
+            print(e.get_context(input_data))
+            exit(1)
+        return tree
+
+
+def main():
+    parser = Parser()
+    expr = ''.join(sys.argv[1:])
+    tree = parser.parse(expr)
+    print(tree)
+
+
+main()
