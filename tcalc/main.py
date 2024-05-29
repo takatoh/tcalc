c2094140d2680a74e79a3b8b353e6af1b07f76a3
takatoh <takatoh.m@gmail.com>
2024-05-30 07:11:58 +0900

    tcalc/main.py: Use tcalc.parser.Calculator class.

--- tcalc/main.py
+++ tcalc/main.py
@@ -1,12 +1,5 @@
-from tcalc.parser import Parser
-from os import path
+from tcalc.parser import Parser, Calculator
 import sys
-
-
-here = path.dirname(path.abspath(__file__))
-grammer_file = path.join(here, '../grammer/tcalc.lark')
-with open(grammer_file) as f:
-    TCALC_GRAMMER = f.read()
 
 
 def main():
@@ -14,6 +7,5 @@ def main():
     expr = ''.join(sys.argv[1:])
     tree = parser.parse(expr)
     print(tree)
-
-
-main()
+    calculator = Calculator()
+    calculator.visit(tree)
