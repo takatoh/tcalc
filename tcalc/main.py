64cf0a2740543a5796e47d4de792677f788a2fae
takatoh <takatoh.m@gmail.com>
2024-06-09 17:09:15 +0900

    tcalc/main.py: --help and --version options.

--- tcalc/main.py
+++ tcalc/main.py
@@ -1,11 +1,37 @@
 from tcalc.core import Parser, Calculator
-import sys
+from tcalc import __version__
+import argparse
+
+
+SCRIPT_VERSION = f'v{__version__}'
 
 
 def main():
+    args = parse_arguments()
     parser = Parser()
-    expr = ''.join(sys.argv[1:])
+    expr = ''.join(args.terms)
     tree = parser.parse(expr)
     calculator = Calculator()
     answer = calculator.visit(tree)
     print(answer)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Time calculator'
+    )
+    parser.add_argument(
+        'terms',
+        action='store',
+        nargs='+',
+        metavar='TERM',
+        help='term or operator'
+    )
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=SCRIPT_VERSION,
+        help='show version and exit'
+    )
+    args = parser.parse_args()
+    return args
